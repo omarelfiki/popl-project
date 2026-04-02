08bd9a0214834fed8553f8ceb056cfa03a7d1930
Omar Elfiki <omarelfiki@gmail.com>
2026-04-02 17:33:07 +0200

    Add sealed interface and records for expression types

--- src/main/java/AST/expression.java
+++ src/main/java/AST/expression.java
@@ -4,4 +4,9 @@ package AST;
 /*
     Expressions evaluate to return a value but don't change state.
     */
-sealed interface expression permits operation, assignment {}
+sealed interface expression permits number, variable, binary, unary {}
+
+record number(double variable) implements expression {}
+record variable(String name) implements expression {}
+record binary(expression left, String op, expression right) implements expression {}
+record unary(String op, expression expr) implements expression {}
