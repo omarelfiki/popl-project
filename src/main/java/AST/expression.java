07cf3fe3f5d6dbc10bb404b9aefab4a3ecd261cb
Omar Elfiki <omarelfiki@gmail.com>
2026-04-09 11:54:56 +0200

    Add comments in expression interface

--- src/main/java/AST/expression.java
+++ src/main/java/AST/expression.java
@@ -1,14 +1,13 @@
 package AST;
 
-
-/*
-    Expressions evaluate to return a value but don't change state.
-    */
+/* Expressions evaluate to return a value but don't change state. */
 public sealed interface expression
         permits expression.number, expression.variable, expression.binary, expression.unary {
 
     record number(double value) implements expression {}
     record variable(String name) implements expression {}
+
+    // Binary and Unary records self-reference the expression interface to allow for nested expressions
     record binary(expression left, String op, expression right) implements expression {}
     record unary(String op, expression expr) implements expression {}
 }
